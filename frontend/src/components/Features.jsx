f019df872396a58d36b0d3306e129de0c47a48bf
Romtouf <romtouf@laposte.net>
2023-11-09 16:06:36 +0100

    Correction d'une erreur de class en className + cursor pointer sur les boutons d'édition

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -10,7 +10,7 @@ const Features = () => {
         <section className="features">
           <h2 className="sr-only">Features</h2>
           <div className="feature-item">
-            <img src={iconChat} alt="Chat Icon" class="feature-icon" />
+            <img src={iconChat} alt="Chat Icon" className="feature-icon" />
             <h3 className="feature-item-title">You are our #1 priority</h3>
             <p>
               Need to talk to a representative? You can get in touch through our
