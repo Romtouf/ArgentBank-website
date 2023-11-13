616ca6e210a29ca46bfd2f68fb3dc6746d7a6f99
Romtouf <romtouf@laposte.net>
2023-11-13 09:46:01 +0100

    Correction json

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../style/pages/home.css";
-import data from '../data/features.json';
+import dataFeatures from '../data/features.json';
 import Banner from '../components/Banner';
 import Features from '../components/Features';
 
@@ -10,9 +10,9 @@ const Home = () => {
             <Banner />
            <section className="features">
                 <h2 className="sr-only">Features</h2>
-                {data.map(({ id, logo, description, title, text }) => (
+                {dataFeatures.map(({ id, logo, description, title, text }) => (
                 <Features key={id} image={logo} alt={description} title={title} text={text} />
-                ))};
+                ))}
             </section>
         </>
     );
